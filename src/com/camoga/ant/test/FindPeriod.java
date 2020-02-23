69daa89354555aeed8d2caa1ce47cd455d9a1ef4
MrCamoga <mrcamoga@gmail.com>
2020-02-23 16:14:27 +0100

    Approximate highway period Knowing the size of the highway and the steps it takes the ant to move in one direction you can approximate the period of the highway Example: The ant LLRLRRLLRLLLLLRRLLLRRLRLR takes approximately 4575.22 steps to move one cell to the right The width of the highway is 6201720 Period ≈ 6201720*4575.22= 28374233378 (it'd take 8-10 mins to compute) Real period 			= 28299602536

---
+++ src/com/camoga/ant/test/FindPeriod.java
@@ -0,0 +1,48 @@
+package com.camoga.ant.test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashSet;
+
+import javax.imageio.ImageIO;
+
+public class FindPeriod {
+	public static void main(String[] args) throws Exception {
+		highwayWidth("LLRLRRLLRLLLLLRRLLLRRLRLR.png");
+	}
+	
+	public static void highwayWidth(String path) throws Exception {
+		BufferedImage image = ImageIO.read(new File(path));
+		int width = image.getWidth(), height = image.getHeight();
+		int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+		HashSet<Integer> subperiods = new HashSet<Integer>(); //Subperiod is the period of one row
+		boolean unknown = false;
+		h:for(int y = 0; y < height; y++) {
+			p:for(int period = 1; period < width*0.8; period++) {
+				for(int x = 0; x < width-period; x++) {
+					if(pixels[x+y*width] != pixels[x+period+y*width]) continue p;
+				}
+				subperiods.add(period);
+				continue h;
+			}
+			unknown = true;
+		}
+		System.out.println("Subperiods: " + subperiods);
+		long lcm = lcm(subperiods.stream().mapToInt(e -> e).toArray());
+		if(!unknown) System.out.println("Period = " + lcm);
+		else System.out.println("Period >= " + lcm);
+	}
+	
+	public static long lcm(int[] l) {
+		long result = l[0];
+		for(int i = 1; i < l.length; i++) {
+			result = result*l[i]/gcd(result,l[i]);
+		}
+		return result;
+	}
+	
+	public static long gcd(long a, long b) {
+		if(a==0) return b;
+		return gcd(b%a,a);
+	}
+}
