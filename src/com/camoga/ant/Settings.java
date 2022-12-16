778a6c57fd169b76623ac3577e60186d78bedf74
Carlos Moya <mrcamoga@gmail.com>
2022-12-17 00:27:51 +0100

    changed settings

--- src/com/camoga/ant/Settings.java
+++ src/com/camoga/ant/Settings.java
@@ -4,15 +4,15 @@ public class Settings {
 	
 	//GUI
 	
-	public static int canvasSize = 16; // size of canvas (and output image) in chunks (e.g. scale = 16, cSIZE = 64 => size = 1024x1024)
+	public static int canvasSize = 8; // size of canvas (and output image) in chunks (e.g. scale = 16, cSIZE = 64 => size = 1024x1024)
 	public static boolean followAnt = true;
 	static boolean smoothFollow = false; //TODO
 	public static boolean renderVoid = false; // draws black where no chunk has been generated
-	public static int itpf = 1000001; // iterations between frames
+	public static int itpf = 1000000; // iterations between frames
 	
 	//FIND HIGHWAYS
-	public static int chunkCheck = 70; // Check if the ant forms a highway when the ant goes further than this chunk from the origin
-	public static float repeatcheck = 1000; // Number of times the period has to repeat to confirm that it's correct (e.g. You're more certain that 10101010101010101010 has a period of 2 than 1010)
+	public static int chunkCheck = 100; // Check if the ant forms a highway when the ant goes further than this chunk from the origin
+	public static float repeatpercent = 100; // Number of times the period has to repeat to confirm that it's correct (e.g. You're more certain that 10101010101010101010 has a period of 2 than 1010)
 	static boolean autosave = false;
 	
 	
